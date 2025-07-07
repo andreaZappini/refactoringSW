7ca8e2ee6cca4c59dea2c84fe6d0b5beb2ce718a
andreaZappini <zappini.andrea03@gmail.com>
2025-07-08 01:00:30 +0200

    fine implementazione state

--- src/printer/FormatterSetup.java
+++ src/printer/FormatterSetup.java
@@ -1,19 +1,15 @@
 package printer;
 
-import model.Configuratore;
 import model.Elenco;
-import model.Fruitore;
 import model.Luogo;
 import model.TipoVisita;
 import model.Utente;
 import model.Visita;
-import model.Volontario;
-
 public class FormatterSetup {
     public static void setup(){
-        FormatterRegister.registerPrinter(Volontario.class, new VolontarioPrinter());
-        FormatterRegister.registerPrinter(Configuratore.class, new ConfiguratorePrinter());
-        FormatterRegister.registerPrinter(Fruitore.class, new FruitorePrinter());
+        // FormatterRegister.registerPrinter(Volontario.class, new VolontarioPrinter());
+        // FormatterRegister.registerPrinter(Configuratore.class, new ConfiguratorePrinter());
+        // FormatterRegister.registerPrinter(Fruitore.class, new FruitorePrinter());
         FormatterRegister.registerPrinter(Utente.class, new UtentePrinter());
         FormatterRegister.registerPrinter(Visita.class, new VisitaPrinter());
         FormatterRegister.registerPrinter(Luogo.class, new LuogoPrinter());
