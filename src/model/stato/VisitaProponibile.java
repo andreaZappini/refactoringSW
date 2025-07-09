94a2954738c794e70e07a6d105de4b1d6ffcd3af
andreaZappini <zappini.andrea03@gmail.com>
2025-07-09 11:45:04 +0200

    caricamento per chiarimenti

--- src/model/stato/VisitaProponibile.java
+++ src/model/stato/VisitaProponibile.java
@@ -5,6 +5,11 @@ import model.Visita;
 
 public class VisitaProponibile implements StatiVisita {
 
+    @Override 
+    public void gestisciTransizione(Visita visita) {
+        visita.setStato(new VisitaProposta());
+    }
+
     @Override
     public String toString() {
 
